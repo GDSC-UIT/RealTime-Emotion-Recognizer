a365485974254be64d65a454271f10bc12e7ddd0
ngctnnnn <ngctnnnn@gmail.com>
2021-09-09 08:58:10 +0700

    win config

--- dependencies/win/win.sh
+++ dependencies/win/win.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip install -r requirements.txt
 pip install git+https://github.com/rcmalli/keras-vggface.git
+pip install matplotlib, pandas, pillow, autopep8, sklean, tensorflow, keras, mtcnn, opencv-python
