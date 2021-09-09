351878b048c5abed2942f713656dadc59df32b72
ngctnnnn <ngctnnnn@gmail.com>
2021-09-09 20:42:33 +0700

    fix bug

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Conv2D,Flatten,MaxPool2D,BatchNormalization,GlobalAveragePooling2D, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import Sequential
 
 #Create model
 model = Sequential()
