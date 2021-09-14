4f8dd469a02998e97c9db42c58d1dd93daaced8e
ngctnnnn <ngctnnnn@gmail.com>
2021-09-14 16:48:20 +0700

    finish

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ while True:
 
     for (x,y,w,h) in faces_detected:
         
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0))
+        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0), thickness=4)
 
         roi_gray = gray_img[y:y+w, x:x+h]
         roi_gray = cv2.resize(roi_gray, (48,48))
@@ -52,7 +52,7 @@ while True:
         cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (219,68,55), 2)
 
     resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ',resized_img)
+    cv2.imshow(resized_img)
 
     if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
         break
