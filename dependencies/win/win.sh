7e5748d88160916e553dac953476bb74fd0490f2
ngctnnnn <ngctnnnn@gmail.com>
2021-09-15 08:24:56 +0700

    change rqm

--- dependencies/win/win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pip install git+https://github.com/yaledhlab/vggface.git
-pip install matplotlib pandas pillow autopep8 sklearn tensorflow keras mtcnn opencv-python seaborn scikit-image scikit-plot
